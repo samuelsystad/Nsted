b735ed9140ef4095ddf25311678ea974d7147142
Chamundsen <simencamu@hotmail.com>
2023-10-10 13:37:35 +0200

    Revert "Lagt til HTTP sikkerhet headers"
    This reverts commit d0d0049f02707a19fc09f1514de9f1301982884e.

--- Nsted/Program.cs
+++ Nsted/Program.cs
@@ -1,11 +1,7 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
@@ -24,13 +20,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-// This is the default route for MVC apps
 
 app.Run();
-
-

7bf6d9c8dc72fbb4033237beaa4dab8386119b9c
Chamundsen <simencamu@hotmail.com>
2023-10-23 21:17:50 +0200

    Logg inn siden

--- Nsted/Controllers/HomeController.cs
+++ Nsted/Controllers/HomeController.cs
@@ -54,9 +54,14 @@ namespace Nsted.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
 
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });

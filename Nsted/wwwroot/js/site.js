dbea9dc8cb4e9043efa0eec2c523b289dcb61ead
Chamundsen <simencamu@hotmail.com>
2023-10-24 21:54:46 +0200

    Lagt til FerdigeOrdrer side

--- Nsted/wwwroot/js/site.js
+++ Nsted/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $(".overlay-button").click(function () {
+        var overlayContent = $(this).closest('.description-cell').find('.overlay-content');
+        overlayContent.toggle();
+    });
+});

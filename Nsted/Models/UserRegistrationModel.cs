b446979dd299be12569b54e2e7ebfd7490df7e06
andreaswiv <andreaswiv@uia.no>
2023-11-18 16:54:35 +0100

    gjorde ferdig log inn og registrering

---
+++ Nsted/Models/UserRegistrationModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nsted.Models
+{
+    public class UserRegistrationModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
